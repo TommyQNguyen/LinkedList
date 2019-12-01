d13501a8077f11fbea28363f9304eb9a5a4bc155
tomtomplusplus <tommy_361@hotmail.com>
2019-11-30 19:16:36 -0500

    Fonction a completer

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -4,9 +4,26 @@
 
 using namespace std;
 
+//Global variables
 const int MENU_ADD = 1, MENU_DELETE = 2, MENU_SHOW_ELEMENT = 3, MENU_QUIT = 4;
 
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+Node* head = NULL;
+Node* tail = NULL;
+
+//Function prototypes
 int showMenu();
+void createFirstNode(int value);
+void showNodeList();
+void insertNodeAtBeginning(int value);
+void insertNodeAtEnd(int value);
+void insertNodeAtGivenPosition(int position, int value);
+
 
 int main()
 {
@@ -41,15 +58,76 @@ int showMenu() {
 	int choice;
 	cout << "A faire: Affichage de la liste au complet!\n\n";
 
-	cout << endl << endl <<
+	cout <<
 		"Que voulez-vous faire?\n\n" <<
-		"1 Ajouter un élément à la liste?\n" <<
-		"2 Retirer un élément de la liste?\n" <<
-		"3 Afficher un élément de la liste?\n" <<
-		"4 Quitter?\n\n" <<
-		"Il y a présentement N élément(s) dans la liste\n\n\n\n" <<
+		"1 Ajouter un element à la liste?\n" <<
+		"2 Retirer un element de la liste?\n" <<
+		"3 Afficher un element de la liste?\n" <<
+		"4 Quitter?\n\n\n" <<
+		"Il y a présentement N element(s) dans la liste\n\n\n\n" <<
 		"Votre choix : ";
 	cin >> choice;
 	clrscr();
 	return choice;
+}
+
+
+void createFirstNode(int value)
+{
+	Node* temp = new Node;
+	temp->data = value;
+	temp->next = NULL;
+
+	if (head == NULL)
+	{
+		head = temp;
+		tail = temp;
+		temp = NULL;
+	}
+	else
+	{
+		tail->next;
+		tail = temp;
+	}
+}
+
+void showNodeList()
+{
+	Node* temp = head;
+
+	while (temp != NULL)
+	{
+		cout << temp->data << "\t";
+		temp = temp->next;
+	}
+}
+
+void insertNodeAtBeginning(int value)
+{
+	Node* temp = new Node;
+	temp->data = value;
+	temp->next = head;
+	head = temp;
+}
+
+void insertNodeAtEnd(int value)
+{
+	createFirstNode(value);
+}
+
+void insertNodeAtGivenPosition(int position, int value)
+{
+	Node* temp = new Node;
+	Node* previous = new Node;
+	Node* current = head;
+
+	for (int i = 0; i < position; i++)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	temp->data = value;
+	previous->next = temp;
+	temp->next = current;
 }
